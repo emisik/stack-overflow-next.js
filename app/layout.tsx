e2c4dd2296be4985c0c9349f3722ab2e2b8b9dcb
Edith <edithmisik@gmail.com>
2023-09-30 05:21:24 +0300

    Addition of layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,50 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import React from 'react'
+import { ClerkProvider } from "@clerk/nextjs";
+import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
 
-export const metadata = {
-  title: 'Next.js 13 with Clerk',
-}
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community driven platform for asking and answering programmin questions.Get help,share knowledge and collaborate with developers from around the world.Explore topics in web development,mobile app development,algorithms,data structures and much more.",
+  icons: {
+    icon: "/public/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">DevFlow again</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
